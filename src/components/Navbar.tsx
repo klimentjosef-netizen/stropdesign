25de56d3d472f53705bb4924614d6a22970ea89c
Josef Kliment <josefkliment@Josef-MacBook-Air.local>
2026-03-23 12:21:44 +0100

    fix: větší logo (90px)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -26,7 +26,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-6 lg:px-10 flex items-center justify-between h-[68px]">
         {/* Logo */}
         <Link href={useLocalePath("/")}>
-          <Logo variant="dark" size={70} />
+          <Logo variant="dark" size={90} />
         </Link>
 
         {/* Desktop links */}
